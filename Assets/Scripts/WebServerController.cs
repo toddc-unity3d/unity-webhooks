d9329cbee13d47f634ed5cbc8bc9612d297d5f37
Todd Carr <toddc@unity3d.com>
2020-09-28 15:58:20 -0700

    Added WebServerController script

---
+++ Assets/Scripts/WebServerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class WebServerController : MonoBehaviour
+{
+    HttpListener listener;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!HttpListener.IsSupported)
+        {
+            Debug.Log("HttpListener not supported!");
+            return;
+        }
+
+        HttpListener listener = new HttpListener();
+
+
+        Debug.Log("Server started...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
